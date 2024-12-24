99a1398bc83f159d9b9ee3039b76347e7770b406
captnced <cedric.elsaesser1603@gmail.com>
2024-12-24 23:57:26 +0100

    Initial Commit

---
+++ Patches.cs
@@ -0,0 +1,26 @@
+﻿using BulwarkStudios.Stanford.Demo;
+using HarmonyLib;
+
+namespace NoSeizureWarning;
+
+public class Patches
+{
+    [HarmonyPatch(typeof(GameInitialize), nameof(GameInitialize.Start))]
+    public static class BuildingInstancePatcher
+    {
+        public static void Postfix(GameInitialize __instance)
+        {
+            foreach (var v in __instance.steps)
+                if (v.waitDuration.Equals(4))
+                {
+                    v.waitDuration = 2;
+                }
+                else if (v.waitDuration.Equals(30))
+                {
+                    v.fadeInDuration = 0;
+                    v.waitDuration = 0;
+                    v.fadeOutDuration = 0;
+                }
+        }
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,26 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using BepInEx.Unity.IL2CPP;
+using HarmonyLib;
+
+namespace NoSeizureWarning;
+
+[BepInPlugin(Guid, Name, Version)]
+[BepInProcess("IXION.exe")]
+public class Plugin : BasePlugin
+{
+    private const string Guid = "captnced.NoSeizureWarning";
+    private const string Name = "No Seizure Warning";
+    private const string Version = "1.0.0";
+    internal new static ManualLogSource Log;
+
+    public override void Load()
+    {
+        Log = base.Log;
+        var harmony = new Harmony(Guid);
+        harmony.PatchAll();
+        foreach (var patch in harmony.GetPatchedMethods())
+            Log.LogInfo("Patched " + patch.DeclaringType + ":" + patch.Name);
+        Log.LogInfo("Loaded \"" + Name + "\" version " + Version + "!");
+    }
+}
