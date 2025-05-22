dde1bcb7eaaec94d76759bbe6bf027eb4aeb8966
captnced <cedric.elsaesser1603@gmail.com>
2025-05-22 21:46:07 +0200

    Made compatible with IMHelper ModMenu

--- Plugin.cs
+++ Plugin.cs
@@ -2,25 +2,51 @@
 using BepInEx.Logging;
 using BepInEx.Unity.IL2CPP;
 using HarmonyLib;
+using IMHelper;
 
 namespace NoSeizureWarning;
 
 [BepInPlugin(Guid, Name, Version)]
 [BepInProcess("IXION.exe")]
+[BepInDependency("captnced.IMHelper", BepInDependency.DependencyFlags.SoftDependency)]
 public class Plugin : BasePlugin
 {
     private const string Guid = "captnced.NoSeizureWarning";
     private const string Name = "No Seizure Warning";
-    private const string Version = "1.0.0";
+    private const string Version = "1.1.0";
     internal new static ManualLogSource Log;
+    private static bool enabled = true;
+    private static Harmony harmony;
 
     public override void Load()
     {
         Log = base.Log;
-        var harmony = new Harmony(Guid);
+        harmony = new Harmony(Guid);
+        if (IL2CPPChainloader.Instance.Plugins.ContainsKey("captnced.IMHelper")) enabled = ModsMenu.isSelfEnabled();
+        if (!enabled)
+            Log.LogInfo("Disabled by IMHelper!");
+        else
+            init();
+    }
+    
+    private static void init()
+    {
         harmony.PatchAll();
         foreach (var patch in harmony.GetPatchedMethods())
             Log.LogInfo("Patched " + patch.DeclaringType + ":" + patch.Name);
         Log.LogInfo("Loaded \"" + Name + "\" version " + Version + "!");
+    }
+
+    private static void disable()
+    {
+        harmony.UnpatchSelf();
+        Log.LogInfo("Unloaded \"" + Name + "\" version " + Version + "!");
+    }
+    
+    public static void enable(bool value)
+    {
+        enabled = value;
+        if (enabled) init();
+        else disable();
     }
 }
